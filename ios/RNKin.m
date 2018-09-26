07c846e2393e0e17775af428abbed266eb6d2a9a
phips28 <phips28@gmx.at>
2018-09-26 16:24:24 +0200

    demo app

--- ios/RNKin.m
+++ ios/RNKin.m
@@ -10,4 +10,39 @@
 }
 RCT_EXPORT_MODULE()
 
+/**
+ * Initialize a new kin instance or ignore if currently exists.
+ * @return
+ */
+RCT_EXPORT_METHOD(initializeApp:
+    (NSString *) appName
+            options:
+            (NSDictionary *) options
+            callback:
+            (RCTResponseSenderBlock) callback) {
+
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        FIRApp *existingApp = [FIRApp appNamed:appName];
+
+        if (!existingApp) {
+            FIROptions *firOptions = [[FIROptions alloc] initWithGoogleAppID:[options valueForKey:@"appId"] GCMSenderID:[options valueForKey:@"messagingSenderId"]];
+
+            firOptions.APIKey = [options valueForKey:@"apiKey"];
+            firOptions.projectID = [options valueForKey:@"projectId"];
+            firOptions.clientID = [options valueForKey:@"clientId"];
+            firOptions.trackingID = [options valueForKey:@"trackingId"];
+            firOptions.databaseURL = [options valueForKey:@"databaseURL"];
+            firOptions.storageBucket = [options valueForKey:@"storageBucket"];
+            firOptions.androidClientID = [options valueForKey:@"androidClientId"];
+            firOptions.deepLinkURLScheme = [options valueForKey:@"deepLinkURLScheme"];
+            firOptions.bundleID = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleIdentifier"];
+
+            [FIRApp configureWithName:appName options:firOptions];
+        }
+
+        callback(@[[NSNull null], @{@"result": @"success"}]);
+    });
+}
+
+
 @end
