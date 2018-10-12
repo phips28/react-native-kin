9656b353659915227e5729e68432a5f9a15c6606
phips28 <phips28@gmx.at>
2018-10-12 10:51:18 +0200

    added EventEmitter for android

--- example/demo/android/app/src/main/java/com/demo/MainApplication.java
+++ example/demo/android/app/src/main/java/com/demo/MainApplication.java
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-                    new RNKinPackage()
+                    new RNKinPackage(MainApplication.this)
             );
         }
 
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import {
   NativeModules,
   NativeEventEmitter,
+  DeviceEventEmitter,
+  Platform,
 } from 'react-native';
 
 const { RNKin } = NativeModules;
 
 // instantiate the event emitter
-const events = new NativeEventEmitter(RNKin);
+const events = Platform.select({
+  ios: new NativeEventEmitter(RNKin),
+  android: DeviceEventEmitter,
+});
+console.log('events', events);
 // and expose it
 RNKin.events = events;
 
