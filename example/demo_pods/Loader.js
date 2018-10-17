3a06e50e2c136f1cff309ee068e13f8365c84d18
phips28 <phips28@gmx.at>
2018-10-17 14:42:41 +0200

    demo_pods: install via pods, but proj does not find dependencies

---
+++ example/demo_pods/Loader.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  Text,
+} from 'react-native';
+
+const Loader = (props) => {
+  const { loading, text } = props;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.activityIndicatorWrapper}>
+        <ActivityIndicator animating={loading} />
+        {text && <Text>{text}</Text>}
+      </View>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    flex: 1,
+    zIndex: 200,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    backgroundColor: '#00000040'
+  },
+  activityIndicatorWrapper: {
+    backgroundColor: '#FFFFFF',
+    height: 100,
+    width: 100,
+    zIndex: 500,
+    borderRadius: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  }
+});
+
+export default Loader;
--- example/demo_pods/ios/Dummy.swift
+++ example/demo_pods/ios/Dummy.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+import Alamofire
+import JWT
+
+class RNKin {
+  private var jwtServiceUrl: String?
+  
+  private func signJWT(
+    _ parameters: Parameters,
+    completion: @escaping (Error?, String?) -> Void
+    ) {
+    // use servive url and sign JWT on server
+    Alamofire.request(
+      "\(self.jwtServiceUrl!)/sign",
+      method: .post,
+      parameters: parameters,
+      encoding: JSONEncoding.default
+      )
+      .validate()
+      .responseJSON { response in
+        guard response.result.isSuccess else {
+          print("Error while signing JWT: \(String(describing: response.result.error))")
+          if response.data != nil {
+            do {
+              var json = try JSONSerialization.jsonObject(with: response.data!, options: []) as? [String: Any]
+              json?["alamofireError"] = String(describing: response.result.error)
+              completion(NSError(domain: "Error while signing JWT", code: response.response?.statusCode ?? 500, userInfo: json), nil)
+              return
+            } catch {
+              // do nothing
+              print(error)
+            }
+          }
+          
+          completion(NSError(domain: "Error while signing JWT: \(String(describing: response.result.error))", code: response.response?.statusCode ?? 500, userInfo: nil), nil)
+          return
+        }
+        
+        guard let value = response.result.value as? [String: Any],
+          let jwt = value["jwt"] as? String else {
+            print("JWT not received from sign service")
+            completion(NSError(domain: "JWT not received from sign service", code: 500, userInfo: nil), nil)
+            return
+        }
+        
+        completion(nil, jwt)
+    }
+  }
+}
